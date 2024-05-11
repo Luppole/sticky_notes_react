e8ee6faeb8034d432e1a106e1c2928198dcddd0d
Luppole <itamarperetzofficial@gmail.com>
2024-05-11 15:24:31 +0300

    584

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
     return (
-    <ul className={classes.posts}>
-        <Post author="Itamar" body="Learning ReactJS" />
-        <Post author="Yaniv" body="The Course Isn't Boring"/>
-    </ul>
+    <>
+        <NewPost />
+        <ul className={classes.posts}>
+            <Post author="Itamar" body="Learning ReactJS" />
+            <Post author="Yaniv" body="The Course Isn't Boring"/>
+        </ul>
+    </>
     );
 }
 
