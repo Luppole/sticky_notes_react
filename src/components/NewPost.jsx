2ec0b57fbad14de8ba7cd5062b251492850b20b3
Luppole <itamarperetzofficial@gmail.com>
2024-05-11 15:31:38 +0300

    585

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,11 +1,16 @@
 import classes from './NewPost.module.css';
 
 function NewPost() {
+  function changeBodyHandler(event) {
+    console.log(event.target.value);
+    
+  }
+
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
       </p>
       <p>
         <label htmlFor="name">Your name</label>
