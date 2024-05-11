33aaf18c60b26ead74a9c213ea363dff8cad4d6c
Luppole <itamarperetzofficial@gmail.com>
2024-05-11 17:30:25 +0300

    chore: Update Modal component to include onClose prop and handle click event on backdrop

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,11 +5,15 @@ import Modal from "./Modal";
 import NewPost from "./NewPost";
 
 function PostsList() {
+    const[modalIsVisible, setModalIsVisible] = useState(true);
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
 
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
 
-    function bodyChangeHandeler(event) {
+    function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
     }
 
@@ -17,14 +21,26 @@ function PostsList() {
         setEnteredAuthor(event.target.value);
     }
 
+    // let modalContent;
+    // if(modalIsVisible) {
+    //     modalContent = <Modal onClose={hideModalHandler}>
+    //     <NewPost 
+    //         onBodyChange={bodyChangeHandler} 
+    //         onAuthorChange={authorChangeHandler}
+    //     />
+    // </Modal>;
+    // }
+
     return (
         <>
-            <Modal>
-                <NewPost 
-                onBodyChange={bodyChangeHandeler} 
-                onAuthorChange={authorChangeHandler}
-                />
-            </Modal>
+            {modalIsVisible && (
+                <Modal onClose={hideModalHandler}>
+                    <NewPost 
+                        onBodyChange={bodyChangeHandler} 
+                        onAuthorChange={authorChangeHandler}
+                    />
+                </Modal> 
+            )}
             <ul className={classes.posts}>
                 <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="Yaniv" body="The Course Isn't Boring"/>
