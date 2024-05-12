8eb9a903a301c2569d98ff6291aa2d0450052d88
Luppole <itamarperetzofficial@gmail.com>
2024-05-12 20:15:57 +0300

    Refactor PostsList component to use IP address for fetch URL and display posts from server

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import NewPost from './components/NewPost.jsx'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App />},
+  { path: '/create-post', element: <NewPost/>},
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 )
