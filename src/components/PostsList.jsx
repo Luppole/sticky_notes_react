4099463389e1d5cfd376d85c17155fe859614d1d
Luppole <itamarperetzofficial@gmail.com>
2024-05-12 19:23:23 +0300

    chore: Refactor PostsList component to fetch and display posts from server

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import Modal from "./Modal";
 import NewPost from "./NewPost";
 
-function PostsList({isPosting, onStopPosting}) {
+function PostsList({ isPosting, onStopPosting }) {
     const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        async function fetchPosts() {
+            const response = await fetch('http://localhost:8080/posts')
+            const resData = await response.json();
+            setPosts(resData.posts);
+        }
+
+        fetchPosts();
+    }, []);
 
     function addPostHandler(postData) {
+        fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
         setPosts((prevPosts) => [postData, ...prevPosts]);
     }
     
