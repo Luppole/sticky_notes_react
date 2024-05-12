f63001febd92524d5be8ff3c87c402db2fddf3cb
Luppole <itamarperetzofficial@gmail.com>
2024-05-12 23:51:38 +0300

    Refactor router configuration to use RootLayout component for nested routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,17 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import NewPost from './components/NewPost.jsx'
+import RootLayout from './routes/RootLayout.jsx'
 
 const router = createBrowserRouter([
-  { path: '/', element: <App />},
-  { path: '/create-post', element: <NewPost/>},
-
+  { 
+    path: '/', 
+    element: <RootLayout />, 
+    children: [
+      { path: '/', element: <App />},
+      { path: '/create-post', element: <NewPost/>},
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
