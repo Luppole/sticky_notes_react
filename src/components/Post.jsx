d832ee0c27260d8e91c06a7a9a0ba5b782622b47
Luppole <itamarperetzofficial@gmail.com>
2024-05-13 13:59:14 +0300

    chore: Update dependencies for react_auth project

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,11 +2,13 @@ import { Link } from 'react-router-dom';
 import classes from "./Post.module.css";
 
 function Post({ id, author, body }) {
+
     return (
     <li className={classes.post}>
         <Link to={id}>
             <p className={classes.author}>{author}</p>
             <p className={classes.body}>{body}</p>
+            <p className={classes.id}>{id}</p>
         </Link>
     </li>
     );
